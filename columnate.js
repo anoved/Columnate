2bb159ae0cba12fd34927e93ae7447209e9d7606
Jim <jim.devona@gmail.com>
2021-04-15 16:42:02 -0400

    Reset head
    Attempt to reset head (links, scripts, etc) as well as body to eliminate niggling layout cases.

--- columnate.js
+++ columnate.js
@@ -1,18 +1,4 @@
 function Columnate() {
-    // remove external stylesheets from document
-    var lc = document.getElementsByTagName('link');
-    for (var i = lc.length - 1; i >= 0; i--) {
-        if (lc[i].rel == "stylesheet") {
-            lc[i].remove();
-        }
-    }
-
-    // remove internal stylesheets from document
-    var ic = document.getElementsByTagName('style');
-    for (var k = ic.length - 1; k >= 0; k-- ) {
-        ic[k].remove();
-    }
-
     // apply columnate stylesheets to document
     var LoadStylesheet = function(url) {
         var c = document.createElement('link');
@@ -22,13 +8,17 @@ function Columnate() {
         c.href = url;
         document.getElementsByTagName('head')[0].appendChild(c);
     }
-    LoadStylesheet('//anoved.github.io/Columnate/columnate.css');
-    LoadStylesheet('//anoved.github.io/Columnate/appearance.css');
   
     // callback that will replace document content with readable version
     var MakeReadable = function() {   
         var doclone = document.cloneNode(true);
         var article = new Readability(doclone).parse();
+        // reset head to nothing but our stylesheets
+        document.head.innerHTML = "";
+        LoadStylesheet('//anoved.github.io/Columnate/columnate.css');
+        LoadStylesheet('//anoved.github.io/Columnate/appearance.css');
+        document.title = article.title;
+        // reset body html to nothing but reformatted content  
         document.body.innerHTML = "<h1>"+article.title+"</h1>"+article.content;
     };
 
